98cbeeb087ef0d33274d92105bbcc687b1a2ffdb
Isaac Obuya <Szack4477@gmail.com>
2021-03-14 05:42:14 +0300

    creating beans and testing with different scopes

---
+++ src/main/java/com/isaac/learning/firstproject/Alien.java
@@ -0,0 +1,47 @@
+package com.isaac.learning.firstproject;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope(value = "prototype")
+public class Alien {
+
+    private int aid;
+    private String aname;
+    private String tech;
+
+    public Alien() {
+        super();
+        // track how many times object is created
+        System.out.println("Object created");
+    }
+
+    public int getAid() {
+        return aid;
+    }
+
+    public void setAid(int aid) {
+        this.aid = aid;
+    }
+
+    public String getAname() {
+        return aname;
+    }
+
+    public void setAname(String aname) {
+        this.aname = aname;
+    }
+
+    public String getTech() {
+        return tech;
+    }
+
+    public void setTech(String tech) {
+        this.tech = tech;
+    }
+
+    public void showObject() {
+        System.out.println("Show object");
+    }
+}
--- src/main/java/com/isaac/learning/firstproject/FirstprojectApplication.java
+++ src/main/java/com/isaac/learning/firstproject/FirstprojectApplication.java
@@ -2,12 +2,31 @@ package com.isaac.learning.firstproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class FirstprojectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FirstprojectApplication.class, args);
+		/**
+		 * Inside JVM container, spring creates a special container - Spring container
+		 * Inside Spring container, spring will create different objects i.e. Spring bean
+		 * 2 different scopes when you create a bean i.e;
+		 * - Singleton Scope
+		 * - Prototype scope
+		 * Marking a class with @Component makes it a bean thereby enabling Spring to create its object in Spring container
+		 * The bean is then injected into our application automatically.
+		 * By default, Spring uses the 'Singleton' scope thereby creating an object only once per application lifetime.
+		 * Using prototype does not create an instance of an object by default for you but rather creates it when needed
+		 * We mark the class with @Scope(value="prototype") to enable the prototype scope.
+		 *
+		 */
+		ConfigurableApplicationContext context = SpringApplication.run(FirstprojectApplication.class, args);
+		Alien alien = context.getBean(Alien.class);
+		alien.showObject();
+
+		Alien alien1 = context.getBean(Alien.class);
+		alien1.showObject();
 	}
 
 }
